93c3670fcbe621735ba813e66cb7749be20670c7
Kamyar Salahi <kam.salahi@berkeley.edu>
2024-05-09 01:16:11 -0700

    gutenberg cleanup

--- scripts/ar5iv/process_ar5iv.py
+++ scripts/ar5iv/process_ar5iv.py
@@ -11,11 +11,46 @@
 # from fsspec.callbacks import TqdmCallback
 from markweb import markdown
 import re
+from markweb.markweb import convert_page
+from bs4 import BeautifulSoup
+import markdownify
 
+class Ar5ivMarkdownConverter(markdown.MyMarkdownConverter):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def convert_sub(self, el, text, convert_as_inline):
+        if not text:
+            return ""
+        return f"_{{{text}}}"
+
+    def convert_sup(self, el, text, convert_as_inline):
+        if not text:
+            return ""
+        return f"^{{{text}}}"
+
+    def convert_br(self, el, text, convert_as_inline):
+        if convert_as_inline:
+            return "<br>"
+
+        if self.options['newline_style'].lower() == markdownify.BACKSLASH:
+            return '\\\n'
+        else:
+            return '  \n'
+
+
+def to_markdown(html):
+    if isinstance(html, str):
+        html = BeautifulSoup(html, "html.parser")
+    text = Ar5ivMarkdownConverter().convert_soup(html)
+    # cleanup: replace nbsp as space
+    # this isn't quite right if we preserve html in places, but we currently are not doing that
+    text = text.replace("\xa0", " ")
+    return text
 
 with open("/juice4/scr4/nlp/crfm/markweb/ar5iv/error/0003/nlin0003055.html", "r") as f:
     s = f.read()
     s = s.split("</head>")[1]
     match = re.search(r"""<h(.)*>References""", s)
     s = s[:match.start()]
-    print(markdown.to_markdown(s))
+    print(to_markdown(s))
--- scripts/gutenberg/process_gutenberg.py
+++ scripts/gutenberg/process_gutenberg.py
@@ -3,8 +3,71 @@ import tempfile
 import os
 import fsspec
 import re
+from markweb.markweb import convert_page
+
+TEXT_START_MARKERS = (
+    r"""\*\*\*[\s]*START.*\*\*\*""",
+    r"\*END\*THE SMALL PRINT.*\n",
+    r"This etext was prepared by.*\n",
+    r"E-text prepared by.*\n",
+    r"Produced b.*\ny",
+    r"Distributed Proofreading Team.*\n",
+    r"Proofreading Team at http://www.pgdp.net.*\n",
+    r"http://gallica.bnf.fr.*\n",
+    r"http://archive.org/details/.*\n",
+    r"http://www.pgdp.net.*\n",
+    r"\n.*Internet Archive.*\n",
+    r"material from the Google Print project.*\n",
+    r"\*END THE SMALL PRINT.*\n",
+    r"This etext was produced by.*\n",
+    r"The Project Gutenberg.*\n",
+    r"http://gutenberg.spiegel.de/ erreichbar..*\n",
+    r"Project Runeberg publishes.*\n",
+    r"Beginning of this Project Gutenberg.*\n",
+    r"Project Gutenberg Online Distributed.*\n",
+    r"Gutenberg Online Distributed.*\n",
+    r"the Project Gutenberg Online Distributed.*\n",
+    r"Project Gutenberg TEI.*\n",
+    r"This eBook was prepared by.*\n",
+    r"http://gutenberg2000.de erreichbar.*\n",
+    r"This Etext was prepared by.*\n",
+    r"This Project Gutenberg Etext was prepared by.*\n",
+    r"Gutenberg Distributed Proofreaders.*\n",
+    r"Project Gutenberg Distributed Proofreaders.*\n",
+    r"the Project Gutenberg Online Distributed Proofreading Team.*\n",
+    r"\*\*The Project Gutenberg.*\n",
+    r"\*SMALL PRINT!.*\n",
+    r"More information about this book is at the top of this file..*\n",
+    r"tells you about restrictions in how the file may be used..*\n",
+    r"l'authorization à les utilizer pour preparer ce texte..*\n",
+    r"of the etext through OCR..*\n",
+    r"\*\*\*\*\*These eBooks Were Prepared By Thousands .*\n",
+    r"We need your donations more than ever!.*\n",
+    r"\*\*\*\*\s*SMALL PRINT!.*\n",
+    r'\["Small Print" V..*\n',
+    r'\(http://www.ibiblio.org/gutenberg/.*\n',
+    r'and the Project Gutenberg Online Distributed Proofreading Team.*\n',
+    r'Mary Meehan, and the Project Gutenberg Online Distributed Proofreading.*\n',
+    r'this Project Gutenberg edition..*\n',
+)
 
 
+TEXT_END_MARKERS = (
+    r"""\*\*\*[\s]*END.*\*\*\*""",
+    r"End of\s*([Tt]he|this)? Project Gutenberg",
+    r"Ende dieses Proje[ck]t Gutenberg",
+    r"End of this is COPYRIGHTED",
+    r"Ende dieses Etextes ",
+    r"Ende diese(s)* Project Gutenber",
+    r"\*\*This is a COPYRIGHTED Project Gutenberg Etext",
+    r"Fin de Project Gutenberg",
+    r"The Project Gutenberg Etext of ",
+    r"Ce document fut pr[eé]sent[eé] en lecture",
+    r"More information about this book is at the top of this file.",
+    r"We need your donations more than ever!",
+    r"END OF PROJECT GUTENBERG",
+    r"End of the Project Gutenberg",
+)
 
 if __name__ == "__main__":
     import sys
@@ -24,24 +87,25 @@ if __name__ == "__main__":
         title = "Unknown Title"
     else:
         title = title[1].split("</title>")[0].strip()
-    match_pre = re.search(r"""</pre>""", html)
-    if match_pre:
-        html = html[match_pre.end():]
-    match_start = re.search(r"""\*\*\* START.*\*\*\*""", html)
-    if match_start:
-        html = html[match_start.end():]
-    match_end = re.search(r"""\*\*\* END.*\*\*\*""", html)
-    if match_end:
-        html = html[:match_end.start()]
+    for start_marker in TEXT_START_MARKERS:
+        match = re.search(start_marker, html)
+        if match:
+            html = html[match.end():]
+    for end_marker in TEXT_END_MARKERS:
+        match = re.search(end_marker, html)
+        if match:
+            html = html[:match.start()]
     
-    markdown = markdown.to_markdown(html)
-    print(title)
-    print(markdown)
-    out = {
-            'metadata': {
-                'source': 'project_gutenberg',
-                'domain': domain,
-                'title': title
-            },
-            'text': markdown
-        }
+    md = markdown.to_markdown(html)
+    # print(title)
+    # print(md)
+    # out = {
+    #         'metadata': {
+    #             'source': 'project_gutenberg',
+    #             'domain': domain,
+    #             'title': title
+    #         },
+    #         'text': markdown
+    #     }
+    # out = convert_page(html, url=domain)
+    # print(out["content"])
