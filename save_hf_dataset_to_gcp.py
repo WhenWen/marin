114781cc92d398402af914de777909bdbae73810
Joel Niklaus <me@joelniklaus.ch>
2024-05-15 10:59:56 +0200

    Added information about how to run the script

--- save_hf_dataset_to_gcp.py
+++ save_hf_dataset_to_gcp.py
@@ -1,6 +1,39 @@
-# How to run this file?
-# python3 save_hf_dataset_to_gcp.py tiiuae/falcon-refinedweb
+"""
+This script downloads a HuggingFace dataset and uploads it to Google Cloud Storage.
 
+It can be run locally, but for large datasets it is recommended to run it on a VM on GCP for faster transfer speeds.
+
+How to run this file?
+1. Create a VM on GCP
+2. Connect to the VM
+3. Install Git: `sudo apt-get install git`
+4. Clone the repository: https://github.com/stanford-crfm/marin
+5. Install conda:
+```
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+```
+6. Create a new conda env (use python 3.9 because 3.10 and 3.11 lead to issues when installing the requirements and running the script):
+```
+conda create --name marin python=3.9
+conda activate marin
+```
+7. Install your requirements:
+```
+cd marin
+pip install -r requirements.txt
+```
+6. Start a tmux session:
+```
+sudo apt-get install tmux
+tmux
+```
+7. Run the file and specify the dataset to be downloaded:
+```python3 save_hf_dataset_to_gcp.py tiiuae/falcon-refinedweb```
+"""
 
 import argparse
 import requests
