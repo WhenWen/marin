8ea7bc23e304c667d4694851c5a4b1505f5e410c
mattyding <66889540+mattyding@users.noreply.github.com>
2024-07-16 12:41:07 -0500

    nested sections, cleaning

--- marin/processing/pubmed/convert.py
+++ marin/processing/pubmed/convert.py
@@ -2,41 +2,87 @@ import re
 
 from lxml import etree
 
-# cleaning_regexes
 number_bracket = re.compile("( \[[0-9][0-9,\s-]*\])")
 author_list = re.compile("( \([A-Z][A-Za-z;\,\.\-\s]+ [12][0-9]{3}\))")
 
+
 def clean_text(text):
-    new_text = re.sub(number_bracket, "", text)
-    new_text = re.sub(author_list, "", new_text)
+    new_text = re.sub(number_bracket, "", text)  # remove number brackets
+    new_text = re.sub(author_list, "", new_text)  # remove author lists
     return new_text
+
+
+def process_section(element, processed_set, level=2) -> list[str]:
+    section = {
+        "title": "",
+        "body": [],
+    }
+
+    if element.tag == "sec":
+        title = element.find("title")
+        if title is not None:
+            title_text = "".join(title.itertext())
+            section["title"] = "#" * level + " " + title_text
+        for child in element:
+            if child.tag == "sec":
+                section["body"].extend(process_section(child, processed_set, level + 1))
+            elif child.tag == "p":
+                paragraph_text = "".join(child.itertext()).strip()
+                # dedup
+                if paragraph_text in processed_set:
+                    continue
+                section["body"].append(paragraph_text)
+                processed_set.add(paragraph_text)
+
+    if not section["body"]:
+        return []
+
+    return [section["title"]] + section["body"]
+
 
 def xml2md(xml_str):
     # Set up return text
-    text = ""
+    text = []
     # Parse the XML file
     root = etree.fromstring(xml_str)
-    #root = tree.getroot()
     # Get the title
-    title = root.xpath('//article-title')
+    title = root.xpath("//article-title")
     if len(title) > 0:
-        title_text = ''.join(title[0].itertext())
-        text += ("# " + title_text + "\n")
-    # Find all titles and paragraphs    
-    titles_and_paragraphs = root.xpath('//title | //p[not(ancestor::table-wrap)] | //abstract')
+        title_text = "".join(title[0].itertext())
+        text.append("# " + title_text)
+    # Find all titles and paragraphs
+    titles_and_paragraphs = root.xpath(
+        "//title | //p[not(ancestor::table-wrap)] | //abstract | //sec"
+    )
+    processed_set = set()
+
     # Iterate through titles and paragraphs interleaved
     for element in titles_and_paragraphs:
-        if element.tag == 'title':
-            title_text = ''.join(element.itertext())
-            text += ('\n\n## ' + title_text)
-        elif element.tag == 'p':
-            paragraph_text = ''.join(element.itertext())
-            text += ('\n\n' + paragraph_text)
-        elif element.tag == "abstract":
-            text += ('\n\n' + "## Abstract")
-    # Standardize newlines
-    text = re.sub("[\n]{2,}", "\n\n", text)
-    # Clean text of references and author list
-    text = clean_text(text)
-    return text
 
+        if element.tag == "title" and element.getparent().tag != "sec":
+            title_text = "".join(element.itertext())
+            text.append("## " + title_text)
+
+        elif element.tag == "p":
+            paragraph_text = "".join(element.itertext())
+            # Remove open access attribution statement
+            if "open access" in paragraph_text.lower():
+                continue
+            if paragraph_text in processed_set:
+                continue
+
+            text.append(paragraph_text)
+            processed_set.add(paragraph_text)
+
+        elif element.tag == "abstract":
+            text.append("## Abstract")
+            for child in element:
+                text.extend(process_section(child, processed_set, level=3))
+
+        elif element.tag == "sec":
+            text.extend(process_section(element, processed_set))
+
+    text = "\n\n".join(text)
+    cleaned_text = clean_text(text)
+
+    return cleaned_text
