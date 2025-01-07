4178a10c8b2cd3f99f72cd10e95284904dc239b5
J38 <jebolton@stanford.edu>
2025-01-06 23:58:05 -0800

    new baseline experiment with decontamination

---
+++ experiments/exp108_baseline_plus_decontaminate.py
@@ -0,0 +1,28 @@
+"""
+Train 1.4B model on FineWeb. Add in decontamination of standard benchmarks.
+https://github.com/stanford-crfm/marin/issues/108
+"""
+
+
+from experiments.defaults import default_tokenize, default_train
+from experiments.llama import llama3_tokenizer, llama_1_4b, llama_1_4b_train_config
+from experiments.pretraining_datasets import fineweb_edu
+from marin.execution.executor import executor_main
+
+fineweb_edu_tokenized = default_tokenize(name="fineweb-edu", dataset=fineweb_edu, tokenizer=llama3_tokenizer)
+fineweb_edu_w_decontaminate_model = default_train(
+    name="fineweb-edu-1.4b",
+    tokenized=fineweb_edu_tokenized,
+    model_config=llama_1_4b,
+    train_config=llama_1_4b_train_config,
+)
+
+############################################################
+
+if __name__ == "__main__":
+    executor_main(
+        steps=[
+            fineweb_edu_w_decontaminate_model,
+        ],
+        description="Train 1.4B model on FineWeb. Add in decontamination of standard benchmarks.",
+    )
