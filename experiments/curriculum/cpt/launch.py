5a459aa29228795f68059abf2bf53360a35f80d7
Suhas Kotha <kotha.suhas@gmail.com>
2025-04-26 14:42:46 -0700

    launchs cript

--- experiments/curriculum/cpt/launch.py
+++ experiments/curriculum/cpt/launch.py
@@ -11,7 +11,7 @@ base_steps = {
     "open-web-math": 20000,
     "pubmed": 100,
     "flan": 20,
-    "latxa": 80,
+    "latxa": 400,
     "spj": 2000,
 }
 
@@ -26,22 +26,23 @@ if __name__ == "__main__":
             schedule_type="cosine",
             model_name="meta-llama/Meta-Llama-3.1-8B",
             num_train_steps=int(base_steps[data1_name] * num_data1_repetitions / total_data1_portion),
-            learning_rate=3e-5,
+            learning_rate=lr,
             num_eval=4 if base_steps[data1_name] == 20 else 20,
             num_lm_eval_harness=4,
             num_data1_repetitions=num_data1_repetitions,
             batch_size=128,
-            additional_tags=[f"{data1_name}-replay"],
+            additional_tags=[f"{data1_name}-replay-sweep-4-1"],
             min_lr_ratio=0.0,
-            version_tag=f"-lr-3e-5",
+            version_tag=f"-lr-{lr}",
             warmup_steps=0.05,
             # data_seed=43,
             # weight_decay=weight_decay,
         )
-        for total_data1_portion in [0.9, 0.1]
+        for total_data1_portion in [1.0, 0.75, 0.5, 0.25, 0.1]
         for data1_name in ["latxa"]
-        for data2_name in ["spj", "dclm", "flan", "open-web-math", "pubmed"]
-        for num_data1_repetitions in [1]
+        for data2_name in ["spj"]
+        for num_data1_repetitions in [2]
+        for lr in [3e-5]
     ]
 
     steps = list(chain(*stage_pairs))
